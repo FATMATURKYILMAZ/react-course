1272e41fcd4a1c0e0e564add0eb0adb0bff4d5b1
FATMATURKYILMAZ <fatma_turkyilmaz>
2024-01-24 23:45:59 +0300

    dosyalar localden yüklendi

---
+++ src/Navi.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navi extends Component {
+  render() {//render fonksiyonu componentlerin yeniden kullanılması durumunu sağlar
+    return (
+      <div>
+        <h2>Navi Component</h2>
+      </div>
+    )
+  }
+}
